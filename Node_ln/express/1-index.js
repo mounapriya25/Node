bc8485c1bf5cd4680242e2bd8ef51c2339956f62
Mounapriya kanikireddy <mounapriyakanikireddygari@gmail.com>
2025-04-24 18:01:06 +0530

    Delete Node_ln directory

--- Node_ln/express/1-index.js
+++
@@ -1,9 +0,0 @@
-const express=require('express')
-const appli=express()
-appli.get('/',(req,res)=>{
-    res.send('hello express')
-    console.log('connected.....')
-})
-
-const port =8000
-appli.listen(port)
