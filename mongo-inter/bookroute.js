305521c3c7dc6f001afdf4f5f9ec573b422d85b5
mounapriya25 <mounapriyakanikireddygari@gmail.com>
2025-04-24 19:25:53 +0530

    Untrack additional deleted files

--- mongo-inter/bookroute.js
+++
@@ -1,9 +0,0 @@
-const conr=require("./bookcontroller")
-const exp=require("express")
-const rt=exp.Router()
-
-rt.post("/postbook",conr.crtbook)
-rt.post("/postauth",conr.crtauth)
-rt.get("/get/:id",conr.getbk)
-
-module.exports=rt
